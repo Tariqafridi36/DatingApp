ebfe7bc05aac4efc2cc867ac3650ecca1e7f3d0c
Tariq Iqbal <tariq.iqbal@aksa.com.pk>
2020-05-04 01:25:29 +0500

    end of theme

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
+
 
 import { AppComponent } from './app.component';
 import { ValuesComponent } from './values/values.component';
@@ -23,7 +25,8 @@ import { ErrorInterceptorProvider } from './_services/error-interceptor';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
